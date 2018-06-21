004ad84e0556e09dfc3ad6a34e1fcbce2db62b2b
Benjamin Vibrac <benjamin.vibrac@tech008.abc-objectif.local>
2018-06-21 17:59:42 +0200

    add style and swipe tab

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,60 +1,184 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import * as React from 'react';
+import {Animated, View, Text, StyleSheet} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+import {TabView, TabBar, SceneMap, type Route, type NavigationState} from 'react-native-tab-view';
+import Home from '../screens/HomeScreen';
+import Camera from '../screens/LinksScreen';
+import Settings from '../screens/SettingsScreen';
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+type State = NavigationState <
+    Route < {
+        key: string,
+        icon: string,
+        color: string,
+    } >
+    >;
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
-});
+export default class BottomBarIconExample extends React.Component<*, State> {
+    static title = 'Custom indicator';
+    static backgroundColor = '#263238';
+    static appbarElevation = 4;
 
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
+    state = {
+        index: 0,
+        routes: [
+            {
+                key: 'article',
+                icon: 'ios-home',
+                color: '#F44336',
+            },
+            {
+                key: 'contacts',
+                icon: 'ios-camera',
+                color: '#3F51B5',
+            },
+            {
+                key: 'albums',
+                icon: 'ios-settings',
+                color: '#4CAF50',
+            },
+        ],
+    };
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
-});
+    _handleIndexChange = index =>
+        this.setState({
+            index,
+        });
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link'}
-    />
-  ),
-};
+    _renderIndicator = props => {
+        const {width, position} = props;
+        const inputRange = [
+            0,
+            0.48,
+            0.49,
+            0.51,
+            0.52,
+            1,
+            1.48,
+            1.49,
+            1.51,
+            1.52,
+            2,
+        ];
 
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-});
+        const scale = position.interpolate({
+            inputRange,
+            outputRange: inputRange.map(x => (Math.trunc(x) === x ? 2 : 0.1)),
+        });
+        const opacity = position.interpolate({
+            inputRange,
+            outputRange: inputRange.map(x => {
+                const d = x - Math.trunc(x);
+                return d === 0.49 || d === 0.51 ? 0 : 1;
+            }),
+        });
+        const translateX = position.interpolate({
+            inputRange: inputRange,
+            outputRange: inputRange.map(x => Math.round(x) * width),
+        });
+        const backgroundColor = position.interpolate({
+            inputRange,
+            outputRange: inputRange.map(
+                x => props.navigationState.routes[Math.round(x)].color
+            ),
+        });
 
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
-    />
-  ),
-};
+        return (
+            <Animated.View
+                style={[styles.container, {width, transform: [{translateX}]}]}
+            >
+                <Animated.View
+                    style={[
+                        styles.indicator,
+                        {backgroundColor, opacity, transform: [{scale}]},
+                    ]}
+                />
+            </Animated.View>
+        );
+    };
 
-export default createBottomTabNavigator({
-  HomeStack,
-  LinksStack,
-  SettingsStack,
+    _renderIcon = ({route}) => (
+        <Ionicons name={route.icon} size={24} style={styles.icon}/>
+    );
+
+    _renderBadge = ({route}) => {
+        if (route.key === '2') {
+            return (
+                <View style={styles.badge}>
+                    <Text style={styles.count}>42</Text>
+                </View>
+            );
+        }
+        return null;
+    };
+
+    _renderTabBar = props => (
+        <TabBar
+            {...props}
+            renderIcon={this._renderIcon}
+            renderBadge={this._renderBadge}
+            renderIndicator={this._renderIndicator}
+            style={styles.tabbar}
+        />
+    );
+
+    _renderScene = SceneMap({
+        article: Home,
+        contacts: Camera,
+        albums: Settings,
+    });
+
+    render() {
+        return (
+            <TabView
+                style={this.props.style}
+                navigationState={this.state}
+                renderScene={this._renderScene}
+                renderTabBar={this._renderTabBar}
+                tabBarPosition="bottom"
+                onIndexChange={this._handleIndexChange}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    tabbar: {
+        backgroundColor: '#263238',
+        overflow: 'hidden',
+    },
+    icon: {
+        backgroundColor: 'transparent',
+        color: 'white',
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        opacity: 0.8,
+    },
+    indicator: {
+        width: 48,
+        height: 48,
+        borderRadius: 24,
+        backgroundColor: '#0084ff',
+        margin: 6,
+    },
+    badge: {
+        marginTop: 4,
+        marginRight: 32,
+        backgroundColor: '#f44336',
+        height: 24,
+        width: 24,
+        borderRadius: 12,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 4,
+    },
+    count: {
+        color: '#fff',
+        fontSize: 12,
+        fontWeight: 'bold',
+        marginTop: -2,
+    },
 });
